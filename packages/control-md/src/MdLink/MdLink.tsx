76995bc9c358f469f067581af6174c57cdd0f6e8
Michael Becker <michael@bemit.codes>
2025-07-13 22:20:54 +0200

    md link show open in new for external links

--- packages/control-md/src/MdLink/MdLink.tsx
+++ packages/control-md/src/MdLink/MdLink.tsx
@@ -1,13 +1,15 @@
-import { getElementProps } from '@control-ui/md/getElementProps'
-import React from 'react'
-import Link, { LinkProps } from '@mui/material/Link'
 import { LinkInternal } from '@control-ui/kit/Link'
+import { getElementProps } from '@control-ui/md/getElementProps'
+import IcOpenInNew from '@mui/icons-material/OpenInNew'
+import Link, { LinkProps } from '@mui/material/Link'
+import React from 'react'
 
-export const MdLink: React.ComponentType<React.PropsWithChildren & React.ComponentPropsWithoutRef<'a'> & { currentDomain?: string, linkUnderline?: LinkProps['underline'] }> = (
+export const MdLink: React.ComponentType<React.PropsWithChildren & React.ComponentPropsWithoutRef<'a'> & { currentDomain?: string, linkUnderline?: LinkProps['underline'], showOpenIn?: boolean }> = (
     {
         currentDomain,
         href,
         linkUnderline = 'hover',
+        showOpenIn = true,
         children, ...p
     },
 ) => {
@@ -29,6 +31,16 @@ export const MdLink: React.ComponentType<React.PropsWithChildren & React.Compone
                 {...getElementProps(p)}
             >
                 {children}
+
+                {showOpenIn ?
+                    <IcOpenInNew
+                        fontSize={'inherit'}
+                        sx={{
+                            verticalAlign: 'text-top',
+                            marginLeft: '0.25em',
+                            display: 'inline-block',
+                        }}
+                    /> : null}
             </Link>
     )
 }
